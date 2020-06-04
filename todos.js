29c353122c35f2a7c3abd4f8537b66b4b018b779
Gusthavo Rodrigues <gusthavo.silvasouza@gmail.com>
2020-06-04 00:03:45 -0300

    Create function todos

--- todos.js
+++ todos.js
@@ -1,1 +1,21 @@
-alert('Funfo');
+var listElemt = document.querySelector('#app ul');
+var inputElement = document.querySelector ('#app input');
+var buttonElement = document.querySelector ('#app butoon');
+
+var todos = [
+    'Fazer cafe',
+    'Estudar Java',
+    'Acessaar Comunidade Java'
+];
+
+function renderTodos() {
+    for(todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElemt.appendChild(todoElement);
+    }
+}
+
+renderTodos();
